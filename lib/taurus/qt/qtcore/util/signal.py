104f9090955af9ed2c4150dd7be1c7a5bbe66505
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-05-09 14:15:23 +0200

    Add baseSignal class

---
+++ lib/taurus/qt/qtcore/util/signal.py
@@ -0,0 +1,48 @@
+"""Provide a Signal class for non-QObject objects"""
+
+from PyQt4 import Qt
+from threading import Lock
+from weakref import WeakKeyDictionary
+
+
+def baseSignal(name, *args):
+    """Signal class for non-Qobject objects.
+
+    :param name: signal name (unlike pyqtSignal, name has
+                 to be specified explicitely)
+    :param args: arguments passed to the pyqtSignal
+    """
+    main_lock = Lock()
+    lock_dict = WeakKeyDictionary()
+    classname = name.capitalize() + "Signaller"
+    attrs = {name: Qt.pyqtSignal(*args, name=name)}
+    signaller_type = type(classname, (Qt.QObject,), attrs)
+
+    def get_signal(self):
+        with main_lock:
+            if self not in lock_dict:
+                lock_dict[self] = Lock()
+        with lock_dict[self]:
+            if not hasattr(self, '_signallers'):
+                self._signallers = {}
+            if (name, args) not in self._signallers:
+                self._signallers[name, args] = signaller_type()
+        return getattr(self._signallers[name, args], name)
+
+    doc = "Base signal {}".format(name)
+    return property(get_signal, doc=doc)
+
+
+if __name__ == '__main__':
+
+    class Base(object):
+        test = baseSignal('test', int)
+
+    def print_func(arg):
+        print(arg)
+
+    base1, base2 = Base(), Base()
+    base1.test.connect(print_func)
+    base2.test.connect(print_func)
+    base1.test.emit(1)
+    base2.test.emit(2)
