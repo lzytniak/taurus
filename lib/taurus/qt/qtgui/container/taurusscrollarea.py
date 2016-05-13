bb8fbc60fffef1d928f589ff23911b2c131018af
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-05-13 16:39:55 +0200

    Replace signatures with slots in taurusscrollarea module

--- lib/taurus/qt/qtgui/container/taurusscrollarea.py
+++ lib/taurus/qt/qtgui/container/taurusscrollarea.py
@@ -86,11 +86,11 @@ class TaurusScrollArea(Qt.QScrollArea, TaurusBaseContainer):
     # Public slots for apply/restore changes
     #-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 
-    @Qt.pyqtSignature("applyPendingChanges()")
+    @Qt.pyqtSlot()
     def applyPendingChanges(self):
         self.applyPendingOperations()
 
-    @Qt.pyqtSignature("resetPendingChanges()")
+    @Qt.pyqtSlot()
     def resetPendingChanges(self):
         self.resetPendingOperations()
 
